c33065c14f22fc487d44819889dfaafad567205d
rahat14 <rahatshovo6666@gmail.com>
2019-09-08 23:21:36 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# DoctorHouseDoctor
+ GG
---
+++ app/src/main/java/com/metacodersbd/doctorhousedoctor/MainActivity.java
@@ -0,0 +1,14 @@
+package com.metacodersbd.doctorhousedoctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
